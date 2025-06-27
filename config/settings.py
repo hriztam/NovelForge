92f0f3c51f4f0860204ad1f9cc334d14b0c2104b
hriztam <hritamstark05@gmail.com>
2025-06-27 12:45:57 +0530

    handling the env variables

--- config/settings.py
+++ config/settings.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Settings:
-    GOOGLE_API_KEY = os.getenv("AIzaSyCriYgozr7OGxuVFJdXdZgMBIxmWwHmCg8")
+    GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY", "default_api_key")
     CHROMA_PATH = "./storage/chroma_db"
     SCREENSHOT_DIR = "./storage/screenshots"
     MAX_ITERATIONS = 3
